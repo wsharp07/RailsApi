148cff081ad7dd5a694430c8afbbb78a2f3f40b6
= <wsharp@ltu.edu>
2015-04-15 22:13:49 -0400

    adding the signup form and route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,18 @@
 class UsersController < ApplicationController
+
+  layout 'login', :only => [:new]
+
   def new
+    @user = User.new
+    if @user.save
+      # Handle a successful save.
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+
   end
 end

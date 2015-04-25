fc26db7fc13bca0ce28274d0128a773ecef570fb
= <wsharp@ltu.edu>
2015-04-24 22:11:27 -0400

    minor tweaks

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,16 +19,19 @@ class User < ActiveRecord::Base
 
   ## Methods
 
-  # Returns the hash digest of the given string.
-  def User.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-        BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
-  end
+  ## Class Methods
+  class << self
+    # Returns the hash digest of the given string.
+    def digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+          BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+    end
 
-  # Returns a random token.
-  def User.new_token
-    SecureRandom.urlsafe_base64
+    # Returns a random token.
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
   end
 
   # Remembers a user in the database for use in persistent sessions.

aa9f55a05493f7cddf30bdd33c29a88ea3021bd2
= <wsharp@ltu.edu>
2015-04-26 21:40:24 -0400

    adding authorization

---
+++ test/integration/users_edit_test.rb
@@ -0,0 +1,39 @@
+require 'test_helper'
+
+class UsersEditTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:william)
+  end
+
+  test "unsuccessful edit" do
+    log_in_as(@user)
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    patch user_path(@user), user: { name:  "",
+                                    email: "foo@invalid",
+                                    username: "foo",
+                                    password:              "foo",
+                                    password_confirmation: "bar" }
+    assert_template 'users/edit'
+  end
+
+  test "successful edit with friendly forwarding" do
+    get edit_user_path(@user)
+    log_in_as(@user)
+    assert_redirected_to edit_user_path(@user)
+    name  = "Foo Bar"
+    email = "foo@bar.com"
+    username = "foobar"
+    patch user_path(@user), user: { name:  name,
+                                    email: email,
+                                    username: username,
+                                    password:              "",
+                                    password_confirmation: "" }
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal @user.name,  name
+    assert_equal @user.email, email
+    assert_equal @user.username, username
+  end
+end

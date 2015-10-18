d11e9a0cc13cf30f87b51403139c859d266ac97d
William Sharp <wsharp@ltu.edu>
2015-10-17 21:20:01 -0400

    fixing new time off page to use the new validations

--- app/models/time_off.rb
+++ app/models/time_off.rb
@@ -2,7 +2,7 @@ class TimeOff < ActiveRecord::Base
   belongs_to :request_type
 
   # Validations
-  validates :request_start_date, presence: true, not_in_past: true
+  validates :request_start_date, presence:true, not_in_past: true
   validates :request_end_date, presence: true
   validates :request_type, presence: true
   validates :comments, presence: true, length: { minimum: 5 }

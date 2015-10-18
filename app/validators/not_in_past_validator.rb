34e805b81ca5645a90e1ab51335a568bff4dc3ab
William Sharp <wsharp@ltu.edu>
2015-10-17 21:19:35 -0400

    adding a new validator for not in past date

--- app/validators/not_in_past_validator.rb
+++ app/validators/not_in_past_validator.rb
@@ -1,10 +1,10 @@
 class NotInPastValidator < ActiveModel::EachValidator
+  uses_messages :not_in_past
+
   def validate_each(record, attribute, value)
-    if value.blank?
-      record.errors.add attribute, (options[:message] || "can't be blank")
-    elsif value < DateTime.now.to_date
+    if value < DateTime.now.to_date
       record.errors.add attribute,
-                        (options[:message] || "can't be in the past")
+                        (options[:not_in_past] || "can't be in the past")
     end
   end
 end

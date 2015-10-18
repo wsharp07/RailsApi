b7700832cbed5abf544a7354fa6bd02fc4ce80c2
William Sharp <wsharp@ltu.edu>
2015-10-17 21:35:51 -0400

    greening not_in_past test

--- app/validators/not_in_past_validator.rb
+++ app/validators/not_in_past_validator.rb
@@ -2,7 +2,7 @@ class NotInPastValidator < ActiveModel::EachValidator
   uses_messages :not_in_past
 
   def validate_each(record, attribute, value)
-    if value < DateTime.now.to_date
+    if value != nil && value < DateTime.now.to_date
       record.errors.add attribute,
                         (options[:not_in_past] || "can't be in the past")
     end

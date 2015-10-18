664ba61b7992b2c54a35bffef26298ee57b45f2e
William Sharp <wsharp@ltu.edu>
2015-10-18 17:08:47 -0400

    adding last_name validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,8 @@ class User < ActiveRecord::Base
   before_create :before_create
 
   ## Validators
-  validates :first_name,  presence: true, length: { maximum: 50 }
+  validates :first_name,  presence: true, length: { maximum: 20 }
+  validates :last_name,  presence: true, length: { maximum: 20 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
             format: { with: VALID_EMAIL_REGEX },
